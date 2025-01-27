48277ba4524c570fa45b6743393c6b881d7179e8
theppasiva <theppasiva@gmail.com>
2025-01-27 23:04:49 +0530

    updated roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -15,25 +15,25 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0c706bd2b03de6632 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "$i: $IP_ADDRESS"
+    # IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0c706bd2b03de6632 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    # echo "$i: $IP_ADDRESS"
 
-    #create R53 record, make sure you delete existing record
-    aws route53 change-resource-record-sets \
-        --hosted-zone-id $ZONE_ID \
-        --change-batch '
-        {
-            "Comment": "Creating a record set for cognito endpoint"
-            ,"Changes": [{
-            "Action"              : "CREATE"
-            ,"ResourceRecordSet"  : {
-                "Name"              : "'$i'.'$DOMAIN_NAME'"
-                ,"Type"             : "A"
-                ,"TTL"              : 1
-                ,"ResourceRecords"  : [{
-                    "Value"         : "'$IP_ADDRESS'"
-                }]
-            }
-            }]
-        }'
+    # #create R53 record, make sure you delete existing record
+    # aws route53 change-resource-record-sets \
+    #     --hosted-zone-id $ZONE_ID \
+    #     --change-batch '
+    #     {
+    #         "Comment": "Creating a record set for cognito endpoint"
+    #         ,"Changes": [{
+    #         "Action"              : "CREATE"
+    #         ,"ResourceRecordSet"  : {
+    #             "Name"              : "'$i'.'$DOMAIN_NAME'"
+    #             ,"Type"             : "A"
+    #             ,"TTL"              : 1
+    #             ,"ResourceRecords"  : [{
+    #                 "Value"         : "'$IP_ADDRESS'"
+    #             }]
+    #         }
+    #         }]
+    #     }'
 done
